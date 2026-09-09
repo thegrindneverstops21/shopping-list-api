cb7cf27d76fba5bb3e800973015775a626915ba2
thegrindneverstops21 <ndlovusamjunior21@gmail.com>
2026-09-09 08:31:46 +0200

    implemented error handling

--- node-js-practice/server.js
+++ node-js-practice/server.js
@@ -1,16 +1,22 @@
-import http from 'node:http';
-import { getDataFromDB } from './database/db.js'
+import http from "node:http";
+import { getDataFromDB } from "./database/db.js";
 
-const PORT = 8000
+const PORT = 8000;
 
 const server = http.createServer(async (req, res) => {
-    const destinations = await getDataFromDB();
+  const destinations = await getDataFromDB();
 
-    if(req.url === '/api' && req.method === 'GET') {
-        res.end(JSON.stringify(destinations));
-    }
-})
+  if (req.url === "/api" && req.method === "GET") {
+    res.setHeader("Content-Type", "application/json");
+    res.statusCode = 200;
+    res.end(JSON.stringify(destinations));
+  } else {
+    res.setHeader("Content-Type", "application/json");
+    res.statusCode = 404;
+    res.end(JSON.stringify({ error: "not found", message: "The requested resource was not found." }));
+  }
+});
 
 server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+  console.log(`Server is running on port ${PORT}`);
+});
